7ff4d0d008390bc10e81c5ccc9442badd798a76e
vamen <vivekstarstar@gmail.com>
2016-10-01 15:57:46 +0530

    spechconvertingCoommit

--- app/src/main/java/com/vivekbalachandra/android/reminder/ConstantData.java
+++ app/src/main/java/com/vivekbalachandra/android/reminder/ConstantData.java
@@ -4,6 +4,6 @@ package com.vivekbalachandra.android.reminder;
  * Created by vivek on 1/10/16.
  */
 public class ConstantData {
-    public  static  String speechToTextWatson="452a56e8-097d-4225-99ed-b9b746a6518f";
-    public  static  String speechTotEXTpwdWatson="flFCUAZkerA1";
+    public  static  String speechToText_Usr_Watson="5b97a106-8206-4abc-af76-00450ebd4327";
+    public  static  String speechToText_pwd_Watson="8SMj8Ov2cDfC";
 }
--- app/src/main/java/com/vivekbalachandra/android/reminder/speechReciver.java
+++ app/src/main/java/com/vivekbalachandra/android/reminder/speechReciver.java
@@ -1,26 +1,105 @@
 package com.vivekbalachandra.android.reminder;
 
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+
+import com.ibm.watson.developer_cloud.speech_to_text.v1.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeDelegate;
 
-public class speechReciver extends AppCompatActivity implements {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class speechReciver extends AppCompatActivity   {
+    class NetworkOperatioin extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            service = new SpeechToText();
+            service.setUsernameAndPassword( ConstantData.speechToText_Usr_Watson , ConstantData.speechToText_pwd_Watson);
+
+            options = new RecognizeOptions().contentType("audio/wav")
+                    .continuous(true).interimResults(true).model("en-US_NarrowbandMode");
+            delegate = new BaseRecognizeDelegate() {
+                @Override
+                public void onMessage(SpeechResults speechResults) {
+                   // Log.d(TAG+"result :",speechResults.toString());
+                    parseJson(speechResults.toString());
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    //e.printStackTrace();
+                    Log.d(TAG+"result","error :"+ e);
+                }
+            };
+            InputStream ins = getResources().openRawResource(
+                    getResources().getIdentifier("man1_nb",
+                            "raw", getPackageName()));
+            service.recognizeUsingWebSockets(ins,
+                    options, delegate);
+            return null;
+        }
+    }
+
+    private void parseJson(String s) {
+        try {
+            JSONObject jsonObject=new JSONObject(s);
+            JSONArray res=jsonObject.getJSONArray("results");
+            JSONObject text=res.getJSONObject(0);
+            JSONArray resultArray=text.getJSONArray("alternatives");
+            JSONObject finalTextObject=resultArray.getJSONObject(0);
+            String finalText=finalTextObject.getString("transcript");
+            //Log.d(TAG+"parsed",text.toString());
+            Log.d(TAG + "parsed", finalText);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String TAG=speechReciver.class.getSimpleName();
+    Button listen;
+    SpeechToText service;
+    RecognizeOptions options;
+    BaseRecognizeDelegate delegate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_speech_reciver);
-        SpeechToText service = new SpeechToText();
-        service.setUsernameAndPassword("{"+ ConstantData.speechToTextWatson +"}", "{"+ConstantData.speechTotEXTpwdWatson+"}");
 
-        MediaRecorder mediaRecorder=new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat();
+        listen= (Button) findViewById(R.id.startRecognizing);
 
-        SpeechResults speechResults=service.recognize();
+
+         listen.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 getAudio();
+                 new NetworkOperatioin().execute();
+             }
+         });
+
+
+
     }
+
+    private void getAudio() {
+
+    }
+
+
 }
